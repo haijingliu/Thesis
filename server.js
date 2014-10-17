3790d5c1c1d43d103bf9a2ecdc014d836ec4c5bd
Haijing Liu <hjing6@gmail.com>
2014-10-17 00:02:46 -0400

    odified server.js

--- server.js
+++ server.js
@@ -4,3 +4,17 @@ var Twit =reuqire('twit'),
 ws = reuqire('ws'),
 net =require('net');
 
+
+
+
+
+var T = new Twit({
+
+consumer_key:
+consumer_secret:
+access_token:
+access_token_secret:
+
+
+
+	});
